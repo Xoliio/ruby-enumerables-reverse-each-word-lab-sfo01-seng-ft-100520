715d0a0e0ad917c0b0638cae2ef355f1955ecec5
Inee Ader <xoliio@gmail.com>
2020-09-09 22:14:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 def reverse_each_word (sentence)
   backwards = []
   separated = sentence.split
-  separated.each do |array, string|
+  separated.each do |array|
     binding.pry
     backwards << string.reverse
     end

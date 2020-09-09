5d40cf3fd915fb75fd282f6fc12ded9d09bb4065
Inee Ader <xoliio@gmail.com>
2020-09-09 21:30:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,11 @@ def reverse_each_word (string)
   backwards = []
   separated = []
   separated << string.split
-
   separated.each do |separated, string|
     backwards << separated.reverse
     end
 backwards.join(" ")
-binding.pry
+
 backwards
+binding.pry
 end

60189739707b6ee56595cb4758b437bb8b5d40de
Inee Ader <xoliio@gmail.com>
2020-09-09 21:11:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word (string)
   separated = []
   separated << string.split
 
-  separated do |element|
+  separated.each do |element|
     binding.pry
     backwards << element.reverse
     end
